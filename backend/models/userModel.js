edb93e86edbf8b45dad64f111917d9dca91163a8
milchevdimitar <milchevdimitar@gmail.com>
2025-01-28 05:22:38 +0200

    2801

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    rank: { type: String, required: true, default: `4`},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    moneyspent: {type: Number, required: true},
     cartData: { type: Object, default: {} }
 }, { minimize: false })
 
